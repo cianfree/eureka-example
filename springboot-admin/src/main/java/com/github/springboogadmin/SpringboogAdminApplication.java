10e68110133d4802dbe4ed2e08932e6661752f62
xiajiqiu1990@163.com <xiajiqiu1990@163.com>
2018-08-05 22:06:20 +0800

    增加SpringBootAdmin

---
+++ springboot-admin/src/main/java/com/github/springboogadmin/SpringboogAdminApplication.java
@@ -0,0 +1,12 @@
+package com.github.springboogadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboogAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboogAdminApplication.class, args);
+	}
+}
---
+++ springboot-admin/src/test/java/com/github/springboogadmin/SpringboogAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.springboogadmin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringboogAdminApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
